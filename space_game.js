c12ae8662c33dd1a9fbc9ad3a32b559c3d6fa6c9
Luke McConnell <lukermcconnell@gmail.com>
2021-09-22 23:14:53 -0600

    added score, time, and laser hit. code organization req'd soon

--- space_game.js
+++ space_game.js
@@ -1,5 +1,9 @@
 let keyPress = '';
 let gameInProgress = 0;
+let time = 60;
+let score = 0;
+let transformRegex = /(-*[0-9]+)/g;
+
 let shipX = 200;
 let shipY = 220;
 let shipMinX = -160;
@@ -16,10 +20,11 @@ let UFOX = -180;
 let UFOMinX = -180;
 let UFOMaxX = 530;
 let UFOY = -60;
-let UFOAnimBit = 0;
 let UFODirection = 1;
 // let UFOMinY = -90;
 // let UFOMaxY = 270;
+let UFOElementID = '';
+let UFOHit = 0;
 
 let numberLasersCreate = 5;
 let laserX = 246;
@@ -27,12 +32,10 @@ let laserY = 160;
 let laserMinY = -100;
 let laserMaxY = 270;
 let laserIDsActive = [];
+let laserIsMoving = 0;
 
 let shipComponentents = ['ship-nose', 'ship-body','ship-wing-left','ship-wing-right', 'ship-tail','ship-tail-fire','ship-tail-fire'];
-let keyFrames = ['0','25','50','75','100'];
 let UFOComponents = ['ufo-glass', 'ufo-alien','ufo-alien-eye-left','ufo-alien-eye-right', 'ufo-alien-body','ufo-top','ufo-body-upper', 'ufo-body-lower','ufo-antenna-pole','ufo-antenna-base','ufo-antenna-bead'];
-
-
 
 function startBackgroundAnimation() {
     for(let i = 0; i < numStarsCreate; i++){
@@ -43,14 +46,16 @@ function startBackgroundAnimation() {
 function startGame(){
     if (!gameInProgress){
         console.log("Starting game");
+        score = 0;
+        setScore();
         setShipCSSRootVariables();
         addShip();
         for(let i = 0; i < numberLasersCreate; i++){
-            console.log("Adding Laser");
             addLaser(`laser${i}`);
         }
         addUFO(1);
         document.onkeydown = checkKey;
+        startTimer();
     }
     gameInProgress = 1;
 }
@@ -61,16 +66,27 @@ function quitGame(){
         graphicsContainer.removeChild(graphicsContainer.lastElementChild);
     }
     document.onkeydown = null;
-    UFOX = -140;
-    UFOY = -60;
     numStarsActive = 0;
     gameInProgress = 0;
     startBackgroundAnimation();
+    time = 0;
+    document.getElementById('game-time-value').innerText = time;
 }
 
 function pauseGame(){
 
 }
+
+async function startTimer(){
+    time = 60;
+    let timeElement = document.getElementById('game-time-value');
+    while (time > -1){
+        timeElement.innerText = time;
+        await sleep(1000);
+        time--;
+    }
+}
+
 
 function addStar(starID){
     appendChildToGraphics('star', '',starID, 'graphics');
@@ -126,10 +142,9 @@ function addUFO(numUFOs){
         for(let j = 0; j < UFOComponents.length; j++){
             appendChildToGraphics(UFOComponents[j], ' ufo-component', `${UFOComponents[j]}${i}`, `ufo${i}`);
         }
-        let UFOContainer = document.getElementById(`ufo-container${i}`);
+        UFOElementID = `ufo-container${i}`;
+        let UFOContainer = document.getElementById(UFOElementID);
         UFOContainer.style.transform = `translate(${UFOX}px, ${UFOY}px)`;
-        //UFOX = UFOX + 120;
-        recordUFOPosition(UFOContainer);
         activateUFOAnimation(UFOContainer);
     }
 }
@@ -139,23 +154,16 @@ function activateUFOAnimation(UFOContainer){
     function frame() {
         if (UFOX == UFOMaxX) {
             UFODirection = -1;
-            // clearInterval(id);
         } else if (UFOX == UFOMinX){
             UFODirection = 1;
-            // clearInterval(id);
-        } 
+        } else if (UFOHit){
+            UFOContainer.style.opacity = '0';
+            score++;
+            setScore();
+            clearInterval(id);
+        }
         UFOX += UFODirection;
         UFOContainer.style.transform = `translate(${UFOX}px, ${UFOY}px)`;
-    }
-}
-
-async function recordUFOPosition(UFOContainer){
-    let regex = /(-*[0-9]+)/g;
-    let transformValues = '';
-    while (UFOContainer){
-        await sleep(500);
-        transformValues = UFOContainer.style.transform.match(regex);
-        //console.log(transformValues[0]);
     }
 }
 
@@ -200,8 +208,8 @@ async function fireLaser(){
     let laserIDNumber = laserIDsActive.length;
     let laserID = `laser${laserIDNumber}`
     let laser = document.getElementById(laserID);
-    //recordLaserPosition(laser)
-    activateFireLaserAnimation(laser)
+    activateFireLaserAnimation(laser);
+    checkLaserHit(laser);
     laserIDsActive.push(laserIDNumber);
     await sleep(2000);
     laserIDsActive.shift();
@@ -210,32 +218,47 @@ async function fireLaser(){
  function activateFireLaserAnimation(laser){
     laserX = shipX + 48;
     laserY = shipY;
+    laserIsMoving = 1;
+    laser.style.opacity = '1';
     let id = setInterval(frame, 5);
     function frame() {
         if (laserY == laserMinY) {
+            laserIsMoving = 0;
+            laser.style.opacity = '0';
             clearInterval(id);
         } else {
             laserY--;
             laser.style.transform = `translate(${laserX}px, ${laserY}px)`;
-            //console.log(laser.style.transform);
         }
     }
 }
 
-async function recordLaserPosition(laser){
-    let regex = /(-*[0-9]+)/g;
-    let transformValues = '';
-    while (laser){
-        await sleep(200);
-        transformValues = laser.style.transform.match(regex);
-        console.log("LaserY: " + laser[1]);
+async function checkLaserHit(laser){
+    while(laserIsMoving){
+        await sleep(10);
+        if(laserX > (UFOX) && laserX < (UFOX+100)){
+            console.log("woohoo")
+            if(laserY < (UFOY+20) && laserY > (UFOY-50)){
+                console.log("laserX: " + laserX);
+                console.log("UFOX: " + UFOX);
+                console.log("laserY: " + laserY);
+                console.log("UFOY: " + UFOY);
+                console.log("HIT!");
+                laserIsMoving = 0;
+                UFOHit = 1;
+            }
+        }
     }
+};
+
+function setScore(){
+    document.getElementById('game-score-value').innerText = score;
 }
- 
- function setShipCSSRootVariables(){
+
+function setShipCSSRootVariables(){
     setCSSRootVariable('--ship-translateX', shipX, 'px');
     setCSSRootVariable('--ship-translateY', shipY, 'px');
- }
+}
 
 let setCSSRootVariable = (varName, value, valueString)  => { document.documentElement.style.setProperty(`${varName}`, value + valueString);}  
 
